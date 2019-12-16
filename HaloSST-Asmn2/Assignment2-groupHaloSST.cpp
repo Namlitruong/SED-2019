42244513d34d2a6c8ddbf4b5e54d1eb91f087b9c
tinng81 <tinng801@gmail.com>
2019-12-16 16:14:43 +0700

    Re-factor main

---
+++ HaloSST-Asmn2/Assignment2-groupHaloSST.cpp
@@ -0,0 +1,23 @@
+//##########################################################################################################################################
+//	School of Science & Technology				
+//	EEET2482 - Software Engineering Design		
+//	Asignment 2 - A VIDEO STORE	
+//	Team name: HaloSST							
+//	Member 1: Nam Truong - s3518702				
+//	Member 2: Tin Nguyen - s3607833				
+//	Member 3: Hoang Tran - s3618889				
+//###########################################################################################################################################
+
+//////////////////////////--Built-in libraries--/////////////////////////////
+#include "pch.h"
+#include <iostream>
+
+//////////////////////////--Define Macro--/////////////////////////////
+
+using namespace std;
+
+//Main Program
+int main()
+{
+	return 0;
+}
