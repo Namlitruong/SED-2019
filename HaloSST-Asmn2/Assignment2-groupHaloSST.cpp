b2cd93e3d1c68576f0ce2208d03ae6a49a09328d
tinng81 <tinng801@gmail.com>
2019-12-16 16:37:10 +0700

    Added input menu and exit checker

--- HaloSST-Asmn2/Assignment2-groupHaloSST.cpp
+++ HaloSST-Asmn2/Assignment2-groupHaloSST.cpp
@@ -11,13 +11,54 @@
 //////////////////////////--Built-in libraries--/////////////////////////////
 #include "pch.h"
 #include <iostream>
+#include <string>
+#include <fstream>
 
 //////////////////////////--Define Macro--/////////////////////////////
 
 using namespace std;
 
+//Function prototypes
+bool CheckExit(char *);
+
 //Main Program
 int main()
 {
+	while (1) {
+		char optSel_val[100];
+		cout << "\n___________________________________________________________";
+		cout << "\nWelcome to HaloSST’s video store" << endl;
+		cout << "\nEnter an option below." << endl;
+		cout << "1. Add a new item, update or delete an existing item" << endl;
+		cout << "2. Add new customer or update an existing customer" << endl;
+		cout << "3. Promote an existing customer" << endl;
+		cout << "4. Rent an item" << endl;
+		cout << "5. Return an item" << endl;
+		cout << "6. Display all items" << endl;
+		cout << "7. Display out-of-stock items" << endl;
+		cout << "8. Display all customers" << endl;
+		cout << "9. Display group of customers" << endl;
+		cout << "10. Search items or customers" << endl;
+		cout << "Exit." << endl;
+
+		cin.getline(optSel_val, sizeof(optSel_val) * sizeof(char) );
+		if (CheckExit(iString)) return 0;
+
+	}
+}
+
+/*	Function name: CheckExit.
+	Usage: Check if the input string is Exit or not. If "Exit" is pressed then exit the program and print team information.
+	Input: pointer to input raw string.
+	Output: Boolean flag to acknowledge "Exit" have been pressed or not.
+*/
+bool CheckExit(char * iString) {
+	if (!strcmp(iString, "Exit") || !strcmp(iString, "exit")) { // Consider both Capital (Exit) and non-capital (exit) of the first character to compare with the input string.
+		cout << "ASSIGNMENT 2 - GROUP HaloSST" << endl;
+		cout << "s3518702, s3518702@rmit.edu.vn, Nam Truong" << endl;
+		cout << "s3607833, s3607833@rmit.edu.vn, Tin Nguyen" << endl;
+		cout << "s3618889, s3618889@rmit.edu.vn, Hoang Tran" << endl;
+		return 1;
+	}
 	return 0;
 }
